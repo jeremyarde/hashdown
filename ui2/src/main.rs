9c8026eae2a8c6137eaa41ab81320fd245df771a
Jeremy <jere.arde@gmail.com>
2023-01-03 21:49:25 -0600

    ui2 with tailwind working

--- ui2/src/main.rs
+++ ui2/src/main.rs
@@ -5,17 +5,16 @@ fn main() {
     wasm_logger::init(wasm_logger::Config::default());
     console_error_panic_hook::set_once();
 
-    dioxus::web::launch(app); 
+    dioxus::web::launch_cfg(app, |c| c.into());
 }
 
 fn app(cx: Scope) -> Element {
-
     cx.render(rsx! (
         div {
             style: "text-align: center;",
-            h1 { "🌗 Dioxus 🚀" }
+            h1 { class: "bg-gray-900", "🌗 Dioxus 🚀" }
             h3 { "Frontend that scales." }
-            p { "Dioxus is a portable, performant, and ergonomic framework for building cross-platform user interfaces in Rust." }
+            p { class: "bg-gray-300", "Dioxus is a portable, performant, and ergonomic framework for building cross-platform user interfaces in Rust." }
         }
     ))
 }
