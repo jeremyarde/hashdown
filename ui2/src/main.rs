0b229f9af018ceff30f7ef3f04a65034a1de0778
Jeremy <jere.arde@gmail.com>
2023-01-05 22:52:57 -0600

    add textarea

--- ui2/src/main.rs
+++ ui2/src/main.rs
@@ -1,6 +1,18 @@
-use dioxus::prelude::*;
+use dioxus::{
+    events::oninput,
+    prelude::{dioxus_elements::textarea, *},
+};
+
+use markdownparser::parse_markdown_blocks;
 
 fn app(cx: Scope) -> Element {
+    let model = use_state(&cx, || String::from(""));
+
+    fn send_input(content: String) {
+        print!("Testing in send input");
+        parse_markdown_blocks(content);
+    }
+
     cx.render(rsx! (
         div {
             style: "text-align: center;",
@@ -8,12 +20,17 @@ fn app(cx: Scope) -> Element {
             h3 { "Frontend that scales, I think this is all that is takes." }
             p { class: "bg-blue-600", "This is jeremy testing hot reload, performant, and ergonomic framework for building cross-platform user interfaces in Rust." }
         }
+        div{
+            textarea { rows: "10", cols: "100", oninput: move |e| {model.set(e.value.clone());send_input(e.value.clone());}}
+        }
     ))
 }
 
 fn main() {
     // cargo watch -- cargo run --package ui2 --bin ui2 --target wasm32-unknown-unknown
     // cargo watch -- cargo run --target wasm32-unknown-unknown
+
+    // cargo 
 
     // init debug tool for WebAssembly
     wasm_logger::init(wasm_logger::Config::default());
