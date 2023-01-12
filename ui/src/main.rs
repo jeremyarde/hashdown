8c052802ed9cb01200380b5d1b92d8c68da138b2
Jeremy <jere.arde@gmail.com>
2023-01-11 21:54:54 -0800

    Fix wasm pack error by commenting out some code (too many variables)

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -191,142 +191,142 @@ fn main() {
 fn all_forms(cx: Scope) -> Element {
     cx.render(rsx!(
         div { 
-            div { 
-                class: "md:grid md:grid-cols-3 md:gap-6",
-                div { 
-                    class: "md:col-span-1",
-                    div { 
-                        class: "px-4 sm:px-0",
-                        h3 { 
-                            class: "text-lg font-medium leading-6 text-gray-900",
-                            "Profile"
-                        }
-                        p { 
-                            class: "mt-1 text-sm text-gray-600",
-                            "This information will be displayed publicly so be careful what you share."
-                        }
-                    }
-                }
-                div { 
-                    class: "mt-5 md:col-span-2 md:mt-0",
-                    form { 
-                        action: "#",
-                        method: "POST",
-                        div { 
-                            class: "shadow sm:overflow-hidden sm:rounded-md",
-                            div { 
-                                class: "space-y-6 bg-white px-4 py-5 sm:p-6",
-                                div { 
-                                    class: "grid grid-cols-3 gap-6",
-                                    div { 
-                                        class: "col-span-3 sm:col-span-2",
-                                        label { 
-                                            class: "block text-sm font-medium text-gray-700",
-                                            "Website"
-                                        }
-                                        div { 
-                                            class: "mt-1 flex rounded-md shadow-sm",
-                                            span { 
-                                                class: "inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500",
-                                                "http://"
-                                            }
-                                            input { 
-                                                id: "company-website",
-                                                r#type: "text",
-                                                placeholder: "www.example.com",
-                                                class: "block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm",
-                                                name: "company-website",
-                                            }
-                                        }
-                                    }
-                                }
-                                div { 
-                                    label { 
-                                        class: "block text-sm font-medium text-gray-700",
-                                        "About"
-                                    }
-                                    div { 
-                                        class: "mt-1",
-                                        textarea { 
-                                            id: "about",
-                                            placeholder: "you@example.com",
-                                            class: "mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm",
-                                            name: "about",
-                                            rows: "3",
-                                        }
-                                    }
-                                    p { 
-                                        class: "mt-2 text-sm text-gray-500",
-                                        "Brief description for your profile. URLs are hyperlinked."
-                                    }
-                                }
-                                div { 
-                                    label { 
-                                        class: "block text-sm font-medium text-gray-700",
-                                        "Photo"
-                                    }
-                                    div { 
-                                        class: "mt-1 flex items-center",
-                                        span { 
-                                            class: "inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100",
-                                            // icons::icon_0 {}
-                                        }
-                                        button { 
-                                            r#type: "button",
-                                            class: "ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2",
-                                            "Change"
-                                        }
-                                    }
-                                }
-                                div { 
-                                    label { 
-                                        class: "block text-sm font-medium text-gray-700",
-                                        "Cover photo"
-                                    }
-                                    div { 
-                                        class: "mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6",
-                                        div { 
-                                            class: "space-y-1 text-center",
-                                            // icons::icon_1 {}
-                                            div { 
-                                                class: "flex text-sm text-gray-600",
-                                                label { 
-                                                    class: "relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500",
-                                                    span { 
-                                                        "Upload a file"
-                                                    }
-                                                    input { 
-                                                        id: "file-upload",
-                                                        name: "file-upload",
-                                                        r#type: "file",
-                                                        class: "sr-only",
-                                                    }
-                                                }
-                                                p { 
-                                                    class: "pl-1",
-                                                    "or drag and drop"
-                                                }
-                                            }
-                                            p { 
-                                                class: "text-xs text-gray-500",
-                                                "PNG, JPG, GIF up to 10MB"
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                            div { 
-                                class: "bg-gray-50 px-4 py-3 text-right sm:px-6",
-                                button { 
-                                    r#type: "submit",
-                                    class: "inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2",
-                                    "Save"
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            // div { 
+            //     class: "md:grid md:grid-cols-3 md:gap-6",
+            //     div { 
+            //         class: "md:col-span-1",
+            //         div { 
+            //             class: "px-4 sm:px-0",
+            //             h3 { 
+            //                 class: "text-lg font-medium leading-6 text-gray-900",
+            //                 "Profile"
+            //             }
+            //             p { 
+            //                 class: "mt-1 text-sm text-gray-600",
+            //                 "This information will be displayed publicly so be careful what you share."
+            //             }
+            //         }
+            //     }
+            //     div { 
+            //         class: "mt-5 md:col-span-2 md:mt-0",
+            //         form { 
+            //             action: "#",
+            //             method: "POST",
+            //             div { 
+            //                 class: "shadow sm:overflow-hidden sm:rounded-md",
+            //                 div { 
+            //                     class: "space-y-6 bg-white px-4 py-5 sm:p-6",
+            //                     div { 
+            //                         class: "grid grid-cols-3 gap-6",
+            //                         div { 
+            //                             class: "col-span-3 sm:col-span-2",
+            //                             label { 
+            //                                 class: "block text-sm font-medium text-gray-700",
+            //                                 "Website"
+            //                             }
+            //                             div { 
+            //                                 class: "mt-1 flex rounded-md shadow-sm",
+            //                                 span { 
+            //                                     class: "inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500",
+            //                                     "http://"
+            //                                 }
+            //                                 input { 
+            //                                     id: "company-website",
+            //                                     r#type: "text",
+            //                                     placeholder: "www.example.com",
+            //                                     class: "block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm",
+            //                                     name: "company-website",
+            //                                 }
+            //                             }
+            //                         }
+            //                     }
+            //                     div { 
+            //                         label { 
+            //                             class: "block text-sm font-medium text-gray-700",
+            //                             "About"
+            //                         }
+            //                         div { 
+            //                             class: "mt-1",
+            //                             textarea { 
+            //                                 id: "about",
+            //                                 placeholder: "you@example.com",
+            //                                 class: "mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm",
+            //                                 name: "about",
+            //                                 rows: "3",
+            //                             }
+            //                         }
+            //                         p { 
+            //                             class: "mt-2 text-sm text-gray-500",
+            //                             "Brief description for your profile. URLs are hyperlinked."
+            //                         }
+            //                     }
+            //                     div { 
+            //                         label { 
+            //                             class: "block text-sm font-medium text-gray-700",
+            //                             "Photo"
+            //                         }
+            //                         div { 
+            //                             class: "mt-1 flex items-center",
+            //                             span { 
+            //                                 class: "inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100",
+            //                                 // icons::icon_0 {}
+            //                             }
+            //                             button { 
+            //                                 r#type: "button",
+            //                                 class: "ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2",
+            //                                 "Change"
+            //                             }
+            //                         }
+            //                     }
+            //                     div { 
+            //                         label { 
+            //                             class: "block text-sm font-medium text-gray-700",
+            //                             "Cover photo"
+            //                         }
+            //                         div { 
+            //                             class: "mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6",
+            //                             div { 
+            //                                 class: "space-y-1 text-center",
+            //                                 // icons::icon_1 {}
+            //                                 div { 
+            //                                     class: "flex text-sm text-gray-600",
+            //                                     label { 
+            //                                         class: "relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500",
+            //                                         span { 
+            //                                             "Upload a file"
+            //                                         }
+            //                                         input { 
+            //                                             id: "file-upload",
+            //                                             name: "file-upload",
+            //                                             r#type: "file",
+            //                                             class: "sr-only",
+            //                                         }
+            //                                     }
+            //                                     p { 
+            //                                         class: "pl-1",
+            //                                         "or drag and drop"
+            //                                     }
+            //                                 }
+            //                                 p { 
+            //                                     class: "text-xs text-gray-500",
+            //                                     "PNG, JPG, GIF up to 10MB"
+            //                                 }
+            //                             }
+            //                         }
+            //                     }
+            //                 }
+            //                 div { 
+            //                     class: "bg-gray-50 px-4 py-3 text-right sm:px-6",
+            //                     button { 
+            //                         r#type: "submit",
+            //                         class: "inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2",
+            //                         "Save"
+            //                     }
+            //                 }
+            //             }
+            //         }
+            //     }
+            // }
         }
         div { 
             aria_hidden: "true",
