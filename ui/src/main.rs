1066969ff877870d204b9a53da6224b8db4a2a12
Jeremy <jere.arde@gmail.com>
2023-01-12 22:38:51 -0800

    remove old text area

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -55,11 +55,11 @@ fn Editor(cx: Scope) -> Element {
     cx.render(rsx! {
         div{
             form {
-                textarea { 
-                    rows: "10", cols: "50", oninput: move |e| {send_input(e.value.clone())},
-                    class: "block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
-                    placeholder: "Write your thoughts here..."
-                }
+                // textarea { 
+                //     rows: "10", cols: "50", oninput: move |e| {send_input(e.value.clone())},
+                //     class: "block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+                //     placeholder: "Write your thoughts here..."
+                // }
                 form { 
                     div { class: "w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600",
                         div { class: "flex items-center justify-between px-3 py-2 border-b dark:border-gray-600",
