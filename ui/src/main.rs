e319b91a141e13fbd7d66697f1f42824f7ef6e71
Jeremy <jere.arde@gmail.com>
2023-01-12 22:02:35 -0800

    remove test html

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -105,11 +105,6 @@ fn app(cx: Scope) -> Element {
             class: "wrapper",
             div {
                 style: "text-align: center; display: grid; grid-template-columns: 1f1 min(65ch, 100%) 1fr;",
-                h1 { 
-                    style: "color: red; background-color: green;",
-                    "test regular css in html" }
-                h1 { class: "bg-red-200", "🌗 Dioxus 🚀" }
-                h3 { "Frontend that scales, I think this is all that is takes." }
                 p { class: "bg-lime-600", "This is jeremy testing hot reload, performant, and ergonomic framework for building cross-platform user interfaces in Rust." }
             }
             div{
@@ -168,165 +163,10 @@ fn create_test_data_rsx(cx: Scope, string: String) -> Element {
     })
 }
 
-fn main() {
-    // css
-    // npx tailwindcss -i ./input.css -o ./public/output.css --watch
-
-    // hot reload
-    // cargo watch -- dioxus serve
-    // kill hot reload
-    // sudo lsof -i :8080
-    // 
-
-    // init debug tool for WebAssembly
-    wasm_logger::init(wasm_logger::Config::default());
-    console_error_panic_hook::set_once();
-
-    dioxus::web::launch_cfg(app, |c| c.into());
-}
-
-
-
 
 fn all_forms(cx: Scope) -> Element {
     cx.render(rsx!(
-        div { 
-            // div { 
-            //     class: "md:grid md:grid-cols-3 md:gap-6",
-            //     div { 
-            //         class: "md:col-span-1",
-            //         div { 
-            //             class: "px-4 sm:px-0",
-            //             h3 { 
-            //                 class: "text-lg font-medium leading-6 text-gray-900",
-            //                 "Profile"
-            //             }
-            //             p { 
-            //                 class: "mt-1 text-sm text-gray-600",
-            //                 "This information will be displayed publicly so be careful what you share."
-            //             }
-            //         }
-            //     }
-            //     div { 
-            //         class: "mt-5 md:col-span-2 md:mt-0",
-            //         form { 
-            //             action: "#",
-            //             method: "POST",
-            //             div { 
-            //                 class: "shadow sm:overflow-hidden sm:rounded-md",
-            //                 div { 
-            //                     class: "space-y-6 bg-white px-4 py-5 sm:p-6",
-            //                     div { 
-            //                         class: "grid grid-cols-3 gap-6",
-            //                         div { 
-            //                             class: "col-span-3 sm:col-span-2",
-            //                             label { 
-            //                                 class: "block text-sm font-medium text-gray-700",
-            //                                 "Website"
-            //                             }
-            //                             div { 
-            //                                 class: "mt-1 flex rounded-md shadow-sm",
-            //                                 span { 
-            //                                     class: "inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500",
-            //                                     "http://"
-            //                                 }
-            //                                 input { 
-            //                                     id: "company-website",
-            //                                     r#type: "text",
-            //                                     placeholder: "www.example.com",
-            //                                     class: "block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm",
-            //                                     name: "company-website",
-            //                                 }
-            //                             }
-            //                         }
-            //                     }
-            //                     div { 
-            //                         label { 
-            //                             class: "block text-sm font-medium text-gray-700",
-            //                             "About"
-            //                         }
-            //                         div { 
-            //                             class: "mt-1",
-            //                             textarea { 
-            //                                 id: "about",
-            //                                 placeholder: "you@example.com",
-            //                                 class: "mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm",
-            //                                 name: "about",
-            //                                 rows: "3",
-            //                             }
-            //                         }
-            //                         p { 
-            //                             class: "mt-2 text-sm text-gray-500",
-            //                             "Brief description for your profile. URLs are hyperlinked."
-            //                         }
-            //                     }
-            //                     div { 
-            //                         label { 
-            //                             class: "block text-sm font-medium text-gray-700",
-            //                             "Photo"
-            //                         }
-            //                         div { 
-            //                             class: "mt-1 flex items-center",
-            //                             span { 
-            //                                 class: "inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100",
-            //                                 // icons::icon_0 {}
-            //                             }
-            //                             button { 
-            //                                 r#type: "button",
-            //                                 class: "ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2",
-            //                                 "Change"
-            //                             }
-            //                         }
-            //                     }
-            //                     div { 
-            //                         label { 
-            //                             class: "block text-sm font-medium text-gray-700",
-            //                             "Cover photo"
-            //                         }
-            //                         div { 
-            //                             class: "mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6",
-            //                             div { 
-            //                                 class: "space-y-1 text-center",
-            //                                 // icons::icon_1 {}
-            //                                 div { 
-            //                                     class: "flex text-sm text-gray-600",
-            //                                     label { 
-            //                                         class: "relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500",
-            //                                         span { 
-            //                                             "Upload a file"
-            //                                         }
-            //                                         input { 
-            //                                             id: "file-upload",
-            //                                             name: "file-upload",
-            //                                             r#type: "file",
-            //                                             class: "sr-only",
-            //                                         }
-            //                                     }
-            //                                     p { 
-            //                                         class: "pl-1",
-            //                                         "or drag and drop"
-            //                                     }
-            //                                 }
-            //                                 p { 
-            //                                     class: "text-xs text-gray-500",
-            //                                     "PNG, JPG, GIF up to 10MB"
-            //                                 }
-            //                             }
-            //                         }
-            //                     }
-            //                 }
-            //                 div { 
-            //                     class: "bg-gray-50 px-4 py-3 text-right sm:px-6",
-            //                     button { 
-            //                         r#type: "submit",
-            //                         class: "inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2",
-            //                         "Save"
-            //                     }
-            //                 }
-            //             }
-            //         }
-            //     }
-            // }
+        div {
         }
         div { 
             aria_hidden: "true",
@@ -691,34 +531,21 @@ fn all_forms(cx: Scope) -> Element {
     ))
 }
 
-// mod icons {
-// 	use super::*;
-//     pub(super) fn icon_0(cx: Scope) -> Element {
-//         cx.render(rsx!(
-//             svg { 
-//                 view_box: "0 0 48 48",
-//                 fill: "none",
-//                 class: "mx-auto h-12 w-12 text-gray-400",
-//                 stroke: "currentColor",
-//                 path { 
-//                     stroke_width: "2",
-//                     stroke_linecap: "round",
-//                     stroke_linejoin: "round",
-//                     d: "M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02",
-//                 }
-//             }
-// 		))
-// 	}
-//     pub(super) fn icon_1(cx: Scope) -> Element {
-//         cx.render(rsx!(
-//             svg { 
-//                 class: "h-full w-full text-gray-300",
-//                 fill: "currentColor",
-//                 view_box: "0 0 24 24",
-//                 path { 
-//                     d: "M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z",
-//                 }
-//             }
-// 		))
-// 	}
-// }
+
+fn main() {
+    // css
+    // npx tailwindcss -i ./input.css -o ./public/output.css --watch
+
+    // hot reload
+    // cargo watch -- dioxus serve
+    // kill hot reload
+    // sudo lsof -i :8080
+    // 
+
+    // init debug tool for WebAssembly
+    wasm_logger::init(wasm_logger::Config::default());
+    console_error_panic_hook::set_once();
+
+    dioxus::web::launch_cfg(app, |c| c.into());
+}
+
