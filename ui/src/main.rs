5eb8f0ab3f32329603a3dc1f6bb3fc60d7c84dcd
Jeremy <jere.arde@gmail.com>
2023-01-12 22:23:23 -0800

    change how text box looks

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -54,7 +54,224 @@ fn Editor(cx: Scope) -> Element {
 
     cx.render(rsx! {
         div{
-            textarea { rows: "10", cols: "50", oninput: move |e| {send_input(e.value.clone())}}
+            form {
+                textarea { 
+                    rows: "10", cols: "50", oninput: move |e| {send_input(e.value.clone())},
+                    class: "block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+                    placeholder: "Write your thoughts here..."
+                }
+                form { 
+                    div { class: "w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600",
+                        div { class: "flex items-center justify-between px-3 py-2 border-b dark:border-gray-600",
+                            div { class: "flex flex-wrap items-center divide-gray-200 sm:divide-x dark:divide-gray-600",
+                                div { class: "flex items-center space-x-1 sm:pr-4",
+                                    button { class: "p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600",
+                                        r#type: "button",
+                                        svg { class: "w-5 h-5",
+                                            xmlns: "http://www.w3.org/2000/svg",
+                                            view_box: "0 0 20 20",
+                                            // aria_hidden: "true",
+                                            fill: "currentColor",
+                                            path { 
+                                                d: "M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z",
+                                                clip_rule: "evenodd",
+                                                fill_rule: "evenodd",
+                                            }
+                                        }
+                                        span { class: "sr-only",
+                                            "Attach file"
+                                        }
+                                    }
+                                    button { class: "p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600",
+                                        r#type: "button",
+                                        svg { class: "w-5 h-5",
+                                            fill: "currentColor",
+                                            // aria_hidden: "true",
+                                            view_box: "0 0 20 20",
+                                            xmlns: "http://www.w3.org/2000/svg",
+                                            path { 
+                                                fill_rule: "evenodd",
+                                                d: "M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z",
+                                                clip_rule: "evenodd",
+                                            }
+                                        }
+                                        span { class: "sr-only",
+                                            "Embed map"
+                                        }
+                                    }
+                                    button { class: "p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600",
+                                        r#type: "button",
+                                        svg { class: "w-5 h-5",
+                                            xmlns: "http://www.w3.org/2000/svg",
+                                            fill: "currentColor",
+                                            // aria_hidden: "true",
+                                            view_box: "0 0 20 20",
+                                            path { 
+                                                fill_rule: "evenodd",
+                                                clip_rule: "evenodd",
+                                                d: "M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z",
+                                            }
+                                        }
+                                        span { class: "sr-only",
+                                            "Upload image"
+                                        }
+                                    }
+                                    button { class: "p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600",
+                                        r#type: "button",
+                                        svg { class: "w-5 h-5",
+                                            xmlns: "http://www.w3.org/2000/svg",
+                                            fill: "currentColor",
+                                            view_box: "0 0 20 20",
+                                            // aria_hidden: "true",
+                                            path { 
+                                                clip_rule: "evenodd",
+                                                fill_rule: "evenodd",
+                                                d: "M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z",
+                                            }
+                                        }
+                                        span { class: "sr-only",
+                                            "Format code"
+                                        }
+                                    }
+                                    button { class: "p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600",
+                                        r#type: "button",
+                                        svg { class: "w-5 h-5",
+                                            // aria_hidden: "true",
+                                            fill: "currentColor",
+                                            view_box: "0 0 20 20",
+                                            xmlns: "http://www.w3.org/2000/svg",
+                                            path { 
+                                                fill_rule: "evenodd",
+                                                d: "M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z",
+                                                clip_rule: "evenodd",
+                                            }
+                                        }
+                                        span { class: "sr-only",
+                                            "Add emoji"
+                                        }
+                                    }
+                                }
+                                div { class: "flex flex-wrap items-center space-x-1 sm:pl-4",
+                                    button { class: "p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600",
+                                        r#type: "button",
+                                        svg { class: "w-5 h-5",
+                                            xmlns: "http://www.w3.org/2000/svg",
+                                            // aria_hidden: "true",
+                                            view_box: "0 0 20 20",
+                                            fill: "currentColor",
+                                            path { 
+                                                d: "M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z",
+                                                clip_rule: "evenodd",
+                                                fill_rule: "evenodd",
+                                            }
+                                        }
+                                        span { class: "sr-only",
+                                            "Add list"
+                                        }
+                                    }
+                                    button { class: "p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600",
+                                        r#type: "button",
+                                        svg { class: "w-5 h-5",
+                                            view_box: "0 0 20 20",
+                                            xmlns: "http://www.w3.org/2000/svg",
+                                            // aria_hidden: "true",
+                                            fill: "currentColor",
+                                            path { 
+                                                clip_rule: "evenodd",
+                                                fill_rule: "evenodd",
+                                                d: "M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z",
+                                            }
+                                        }
+                                        span { class: "sr-only",
+                                            "Settings"
+                                        }
+                                    }
+                                    button { class: "p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600",
+                                        r#type: "button",
+                                        svg { class: "w-5 h-5",
+                                            fill: "currentColor",
+                                            view_box: "0 0 20 20",
+                                            // aria_hidden: "true",
+                                            xmlns: "http://www.w3.org/2000/svg",
+                                            path { 
+                                                d: "M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z",
+                                                clip_rule: "evenodd",
+                                                fill_rule: "evenodd",
+                                            }
+                                        }
+                                        span { class: "sr-only",
+                                            "Timeline"
+                                        }
+                                    }
+                                    button { class: "p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600",
+                                        r#type: "button",
+                                        svg { class: "w-5 h-5",
+                                            fill: "currentColor",
+                                            // aria_hidden: "true",
+                                            view_box: "0 0 20 20",
+                                            xmlns: "http://www.w3.org/2000/svg",
+                                            path { 
+                                                d: "M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z",
+                                                clip_rule: "evenodd",
+                                                fill_rule: "evenodd",
+                                            }
+                                        }
+                                        span { class: "sr-only",
+                                            "Download"
+                                        }
+                                    }
+                                }
+                            }
+                            button { class: "p-2 text-gray-500 rounded cursor-pointer sm:ml-auto hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600",
+                                // data_tooltip_target: "tooltip-fullscreen",
+                                r#type: "button",
+                                svg { class: "w-5 h-5",
+                                    // aria_hidden: "true",
+                                    xmlns: "http://www.w3.org/2000/svg",
+                                    fill: "currentColor",
+                                    view_box: "0 0 20 20",
+                                    path { 
+                                        clip_rule: "evenodd",
+                                        d: "M3 4a1 1 0 011-1h4a1 1 0 010 2H6.414l2.293 2.293a1 1 0 11-1.414 1.414L5 6.414V8a1 1 0 01-2 0V4zm9 1a1 1 0 010-2h4a1 1 0 011 1v4a1 1 0 01-2 0V6.414l-2.293 2.293a1 1 0 11-1.414-1.414L13.586 5H12zm-9 7a1 1 0 012 0v1.586l2.293-2.293a1 1 0 111.414 1.414L6.414 15H8a1 1 0 010 2H4a1 1 0 01-1-1v-4zm13-1a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 010-2h1.586l-2.293-2.293a1 1 0 111.414-1.414L15 13.586V12a1 1 0 011-1z",
+                                        fill_rule: "evenodd",
+                                    }
+                                }
+                                span { class: "sr-only",
+                                    "Full screen"
+                                }
+                            }
+                            div { class: "absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700",
+                                id: "tooltip-fullscreen",
+                                role: "tooltip",
+                                "Show full screen"
+                                div { class: "tooltip-arrow",
+                                    // data_popper_arrow: "",
+                                }
+                            }
+                        }
+                        div { class: "px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800",
+                            label { class: "sr-only",
+                                r#for: "editor",
+                                "Publish post"
+                            }
+                            textarea { class: "block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400",
+                                id: "editor",
+                                required: "",
+                                rows: "8",
+                                placeholder: "Write an article...",
+                                oninput: move |e| {send_input(e.value.clone())},
+                                ""
+                            }
+                        }
+                    }
+                    button { class: "inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800",
+                        r#type: "submit",
+                        "Publish post"
+                    }
+                }
+                
+
+            }
             button { 
                 class:"px-5 py-5 font-bold text-white bg-blue-500 rounded hover:bg-blue-700", 
                 onclick: move |_| send_input("1. this is a question\\n  1. option1\\n  2. option 2 here".to_string()), 
@@ -63,6 +280,7 @@ fn Editor(cx: Scope) -> Element {
         }
     })
 }
+
 
 fn Questions(cx: Scope) -> Element {
     let app_state = use_atom_state(&cx, APP);
@@ -105,7 +323,10 @@ fn app(cx: Scope) -> Element {
             class: "wrapper",
             div {
                 style: "text-align: center; display: grid; grid-template-columns: 1f1 min(65ch, 100%) 1fr;",
-                p { class: "bg-lime-600", "This is jeremy testing hot reload, performant, and ergonomic framework for building cross-platform user interfaces in Rust." }
+                h1 { 
+                    class: "bg-lime-200", 
+                    "Write your survey questions in Markdown below" 
+                }
             }
             div{
                 Editor {}
