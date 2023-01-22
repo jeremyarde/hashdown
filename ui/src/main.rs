92439be908ca4576c1e57fc1b3c478fadad192d0
Jeremy <jere.arde@gmail.com>
2023-01-21 18:49:39 -0800

    add new button, fix some formatting css

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -3,13 +3,13 @@ use dioxus::{
     events::{onclick, oninput},
     fermi::use_atom_state,
     prelude::{
-        dioxus_elements::{h5, textarea, fieldset},
+        dioxus_elements::{button, fieldset, h5, textarea},
         *,
     },
 };
 
 // use fermi::{use_atom_ref, use_atom_state, use_set, Atom};
-use markdownparser::{parse_markdown_blocks, Question, Questions, parse_markdown_v3, QuestionType};
+use markdownparser::{parse_markdown_blocks, parse_markdown_v3, Question, QuestionType, Questions};
 
 static APP: Atom<AppState> = |_| AppState::new();
 
@@ -44,6 +44,8 @@ fn Editor(cx: Scope) -> Element {
         log::info!("Recieved input: {content}");
         // let question = parse_markdown_blocks(content.clone());
         let question = parse_markdown_v3(content.clone()).unwrap();
+        log::info!("Questions: {question:#?}");
+
         question_state.modify(|curr| {
             AppState {
                 questions: question,
@@ -65,7 +67,7 @@ fn Editor(cx: Scope) -> Element {
                     let formvalue = e.values.get(FORMINPUT_KEY).clone().unwrap().clone();
                     send_input(formvalue);
                 },
-                div { class: "w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600",
+                div { class: "w-full m-4  border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600",
                     // div { class: "flex items-center justify-between px-3 py-2 border-b dark:border-gray-600",
                     // }
                     div { class: "px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800 focus:ring-red-500",
@@ -81,6 +83,8 @@ fn Editor(cx: Scope) -> Element {
                             name: "forminput"
                             // oninput: move |e| {send_input(e.value.clone())},
                         }
+                        Publish {}
+
                     }
                 }
             }
@@ -88,68 +92,83 @@ fn Editor(cx: Scope) -> Element {
     })
 }
 
+fn Publish(cx: Scope) -> Element {
+    cx.render(rsx! {
+        button {
+            class: "hover:bg-violet-600 w-full text-blue-500 bg-blue-200 rounded p-2",
+            onclick: move |_| {log::info!("Pushed publish :)")},
+            "Publish"
+        }
+    })
+}
 
 fn Questions(cx: Scope) -> Element {
     let app_state = use_atom_state(&cx, APP);
     let editor_state = use_atom_state(&cx, EDITOR);
 
-
-
     cx.render(rsx! {
-            div { 
-                class: "mt-5 md:col-span-2 md:mt-0",
-                form {
-                    // action: "#",
-                    // method: "POST",
-                    div { 
-                        class: "overflow-hidden shadow sm:rounded-md",
-                        div {
-                            class: "space-y-6 bg-white px-4 py-5 sm:p-6",
-                            app_state.questions.iter().map(|q: &Question| rsx!{
-                                fieldset {
-                                    legend { 
-                                        class: "sr-only",
-                                        "{q.text}"
-                                    }
-                                    div { 
-                                        aria_hidden: "true",
-                                        class: "text-base font-medium text-gray-900",
-                                        "{q.text}"
-                                    }
-                                    p {"{q.qtype:?}"}
-                                    {
-                                        // create_questions(q)
-                                        q.options.iter().map(|option| {
-                                            let qtype = match q.qtype {
-                                                QuestionType::Radio => "radio",
-                                                QuestionType::Checkbox => "checkbox",
-                                                QuestionType::Text => "textarea",
-                                            };
+        div {
+            class: "",
+            form {
+                // action: "#",
+                // method: "POST",
+                div {
+                    class: "shadow bg-yellow-100",
+                        app_state.questions.iter().map(|q: &Question| rsx!{
+                            legend {
+                                class: "sr-only",
+                                "{q.text}"
+                            }
+                            div {
+                                aria_hidden: "true",
+                                class: "text-base font-medium text-gray-900",
+                                "{q.text} - {q.qtype:?}"
+                            }
+                            {
+                                // create_questions(q)
+                                q.options.iter().map(|option| {
+                                    let qtype = match q.qtype {
+                                        QuestionType::Radio => "radio",
+                                        QuestionType::Checkbox => "checkbox",
+                                        QuestionType::Text => "textarea",
+                                    };
 
-                                            rsx!{
-                                                li {
-                                                    class: "list-none mt-4 space-y-4  bg-gray-100 hover:bg-gray-200 space-x-2 flex items-start h-5",
-                                                    key: "{option.id}",
-                                                    input { 
-                                                        id: "{option.id}",
-                                                        r#type: "{qtype}",
-                                                        class: "h-4 w-4 rounded border-gray-300 text-yellow-100-600 focus:fill-red-400",
-                                                        name: "{q.id}",
-                                                    }
-                                                }
+                                    rsx!{
+                                        // li {
+                                        //     class: "list-none mt-4 space-y-4  bg-gray-100 hover:bg-gray-200 space-x-2 flex items-start h-5",
+                                        //     key: "{option.id}",
+                                        //     input {
+                                        //         id: "{option.id}",
+                                        //         r#type: "{qtype}",
+                                        //         class: "h-4 w-4 rounded border-gray-300 text-yellow-100-600 focus:fill-red-400",
+                                        //         name: "{q.id}",
+                                        //     }
+                                        // }
+                                        div{
+                                            key: "{option.id}",
+                                            class: "bg-blue-200 focus:fill-red-400",
+                                            input {
+                                                id: "{option.id}",
+                                                name: "{q.id}",
+                                                r#type: "{qtype}",
+                                                class: "bg-gray-100"
                                             }
-                                        })
+                                            label {
+                                                r#for: "{option.id}",
+                                                class: "text-gray-900",
+                                                "{option.text}"
+                                            }
+                                        }
+
                                     }
-                                }
-                            })
-                        }
-                    }
+                                })
+                            }
+                        })
                 }
             }
         }
-    )
+    })
 }
-
 
 fn app(cx: Scope) -> Element {
     let set_app = use_atom_state(&cx, APP);
@@ -157,14 +176,6 @@ fn app(cx: Scope) -> Element {
 
     cx.render(rsx! (
         main{
-            class: "wrapper",
-            div {
-                style: "text-align: center; display: grid; grid-template-columns: 1f1 min(65ch, 100%) 1fr;",
-                h1 { 
-                    class: "bg-lime-200", 
-                    "Write your survey questions in Markdown below" 
-                }
-            }
             div{
                 Editor {}
                 Questions {}
@@ -185,18 +196,18 @@ fn main() {
     // kill -9 PID
 
     /*
-    
-1. question one
-   1. option 1
-   2. option 2
-2. testing
+
+    1. question one
+       1. option 1
+       2. option 2
+    2. testing
 
 
-- this is another
-  - option 1 in other
-- test2 question
-  - this is great
-    */
+    - this is another
+      - option 1 in other
+    - test2 question
+      - this is great
+        */
 
     // init debug tool for WebAssembly
     wasm_logger::init(wasm_logger::Config::default());
@@ -204,4 +215,3 @@ fn main() {
 
     dioxus::web::launch_cfg(app, |c| c.into());
 }
-
