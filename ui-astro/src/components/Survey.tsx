4adbf6c6452718b216f2bc17a6ddf73a5df391d2
Jeremy <jere.arde@gmail.com>
2023-02-09 22:05:21 -0800

    react not rendering properly

--- ui-astro/src/components/Survey.tsx
+++ ui-astro/src/components/Survey.tsx
@@ -1,20 +1,39 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import Checkbox from './Checkbox';
 import Radio from './Radio';
 // import init, { nanoid_gen } from 'markdownparser';
 // import * as test from "markdownparser";
 
 // WebAssembly.instantiate("/markdownparser_bg.wasm");
 
-async function getData() {
-    const data = await fetch('http://localhost:3000/surveys').then((response) => response.json());
-    return data;
-}
+// async function getData() {
+//     const data = await fetch('http://localhost:3000/surveys').then((response) => response.json());
+//     return data;
+// }
 // const wasm = await WebAssembly.instantiateStreaming(fetch('/markdownparser_bg.wasm'));
-
+// let checkboxlist = ['r', 't', 'y']
+// const checkboxDict = {};
+// checkboxlist.forEach((opt) => {
+//     checkboxDict[opt] = false;
+// });
+const initialToppings = {
+    anchovies: false,
+    chicken: false,
+    tomatoes: false,
+}
 // test.nanoid_gen(2);
 export default function Survey() {
     const [survey, setSurvey] = React.useState('');
     const [id, setId] = React.useState('');
+    const [checkbox, setCheckbox] = React.useState({ 'thing': false });
+    const [
+        pizzaToppings,
+        setPizzaToppings
+    ] = React.useState(initialToppings);
+
+    // Get a list of all toppings.
+    // ['anchovies', 'chicken', 'tomato'];
+    const toppingsList = Object.keys(initialToppings);
 
     return (
         <>
@@ -22,6 +41,52 @@ export default function Survey() {
             <Radio
                 radio_options={['a', 'b', 'c']}
             />
+            <Checkbox
+            // options={checkboxDict}
+            // options={checkbox}
+            // setOptinos={setCheckbox}
+            />
+
+
+            <>
+                <form>
+                    <fieldset>
+                        <legend>
+                            Select toppings:
+                        </legend>
+
+                        {/*
+            Iterate over those toppings, and
+            create a checkbox for each one:
+          */}
+                        {toppingsList.map(option => (
+                            <div key={option}>
+                                <input
+                                    type="checkbox"
+                                    id={option}
+                                    value={option}
+                                    checked={pizzaToppings[option] === true}
+                                    onChange={event => {
+                                        setPizzaToppings({
+                                            ...pizzaToppings,
+                                            [option]: event.target.checked,
+                                        })
+                                    }}
+                                />
+                                <label htmlFor={option}>
+                                    {option}
+                                </label>
+                            </div>
+                        ))}
+                    </fieldset>
+                </form>
+                <p>
+                    <strong>Stored state:</strong>
+                </p>
+                <p className="output">
+                    {JSON.stringify(pizzaToppings, null, 2)}
+                </p>
+            </>
         </>
     );
 }
