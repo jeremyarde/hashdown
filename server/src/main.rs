8c17dc5c80491e80c981770d6019bd13661bf7c3
Jeremy <jere.arde@gmail.com>
2023-02-15 00:01:58 -0800

    fixig up tests

--- server/src/main.rs
+++ server/src/main.rs
@@ -81,7 +81,7 @@ where
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // cargo watch -- cargo run
-    ServerApplication::new();
+    ServerApplication::new(false).await;
     Ok(())
 }
 
@@ -91,7 +91,7 @@ struct ServerApplication {
 }
 
 impl ServerApplication {
-    async fn new() -> ServerApplication {
+    async fn new(test: bool) -> ServerApplication {
         const V1: &str = "v1";
 
         dotenvy::from_filename("dev.env").ok();
@@ -119,7 +119,7 @@ impl ServerApplication {
             )
             .layer(TraceLayer::new_for_http());
 
-        let app = configure_app().await;
+        // let app = configure_app().await;
         let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
         tracing::debug!("listening on {}", addr);
 
@@ -144,97 +144,91 @@ impl Drop for ServerApplication {
     }
 }
 
-async fn configure_app() -> Router {
-    const V1: &str = "v1";
-
-    dotenvy::from_filename("dev.env").ok();
-    // initialize tracing
-    tracing_subscriber::fmt::init();
-
-    let db = Database::new(true).await.unwrap();
-    // let ormdb = SqliteConnection::connect(":memory:").await?;
-    // let state = Arc::new(ServerState { db: db });
-    let state = ServerState { db: db };
-
-    // build our application with a route
-    let app = Router::new()
-        .route(&format!("/surveys"), post(create_survey).get(list_survey))
-        .route("/surveys/:id", get(get_survey).post(answer_survey))
-        // .layer(Extension(state))
-        .with_state(state)
-        .layer(
-            CorsLayer::new()
-                .allow_methods([Method::POST, Method::GET])
-                .allow_headers([http::header::CONTENT_TYPE, http::header::ACCEPT])
-                .allow_origin("http://localhost:8080/".parse::<HeaderValue>().unwrap())
-                .allow_origin("http://localhost:8080".parse::<HeaderValue>().unwrap())
-                .allow_origin("http://localhost:3001".parse::<HeaderValue>().unwrap()),
-        )
-        .layer(TraceLayer::new_for_http());
-
-    return app;
-}
-
 #[cfg(test)]
 mod tests {
     use axum::{
         body::Body,
-        http::{self, Request, StatusCode},
-        ServiceExt,
+        http::{self, Request},
     };
-    use serde_json::{json, Value};
+    use mime::Mime;
 
-    use crate::{configure_app, survey::Survey, CreateSurvey, ServerApplication};
+    use crate::{CreateSurvey, ServerApplication};
 
     #[tokio::test]
     async fn list_survey_test() {
         let app = ServerApplication::new().await;
 
-        let get = Request::builder()
-            .method(http::Method::GET)
-            .uri("/surveys")
-            // .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-            .body(Body::from(serde_json::to_string("").unwrap()))
-            .unwrap();
-
-        // let app = configure_app().await;
-        // let response = app
-        //     .oneshot(
-        //         Request::builder()
-        //             .method(http::Method::GET)
-        //             .uri("/surveys")
-        //             // .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-        //             .body(Body::from(serde_json::to_string("").unwrap()))
-        //             .unwrap(),
-        //     )
-        //     .await
+        // let get = Request::builder()
+        //     .method(http::Method::GET)
+        //     .uri("/surveys")
+        //     // .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+        //     .body(Body::from(serde_json::to_string("").unwrap()))
         //     .unwrap();
 
-        println!("response: {response:?}");
+        let mut headers = http::header::HeaderMap::new();
+        // headers.insert(
+        //     // "Content-Type",
+        //     header::CONTENT_TYPE,
+        //     header::HeaderValue::from_static("application/json"),
+        // );
+        // headers.insert(header::CONTENT_ENCODING,
+        // header::)
+        headers.insert("", val);
+
+        let client = reqwest::Client::builder()
+            .default_headers(headers)
+            .build()
+            .unwrap();
+
+        let client_url = format!("http://{}{}", app.base_url.to_string(), "/surveys");
+        println!("Client sending to: {client_url}");
+
+        let response = client
+            .post(client_url)
+            .json(&CreateSurvey {
+                id: "test".to_string(),
+                plaintext: "- another\n - this one".to_string(),
+            })
+            .send()
+            .await
+            .unwrap();
+
+        // client
+        // .post("http://localhost:3000/survey")
+        // .json(&CreateSurvey {
+        //     id,
+        //     plaintext: content,
+        // })
+        // .send()
+        // .await
+
+        // .body(json!({ "id": "test", "plaintext": "- header here\n  - this is a question" }));
+        // client.execute(Request::builder().body(body));
+
+        println!("Response: {response:?}");
         // let create_resp = serde_json::from_slice(response.into_body());
         // assert_eq!(response.status(), StatusCode::OK);
 
-        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
-        let body: Value = serde_json::from_slice(&body).unwrap();
+        // let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        // let body: Value = serde_json::from_slice(&body).unwrap();
 
-        println!("real response: {body:?}");
         // assert_eq!(body, json!({ "data": [1, 2, 3, 4] }));
     }
 
     #[tokio::test]
     async fn create_survey_test() {
-        let app = configure_app().await;
+        // let app = configure_app().await;
+        let app = ServerApplication::new().await;
+        // let response = app.oneshot(get_create_survey_request()).await.unwrap();
 
-        let response = app.oneshot(get_create_survey_request()).await.unwrap();
-
-        println!("response: {response:?}");
+        // println!("response: {response:?}");
         // let create_resp = serde_json::from_slice(response.into_body());
         // assert_eq!(response.status(), StatusCode::OK);
 
-        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
-        let body: Survey = serde_json::from_slice(&body).unwrap();
+        // let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        // let body: Survey = serde_json::from_slice(&body).unwrap();
 
-        println!("real response: {body:?}");
+        // println!("real response: {body:?}");
         // assert_eq!(body, json!({ "data": [1, 2, 3, 4] }));
     }
 
