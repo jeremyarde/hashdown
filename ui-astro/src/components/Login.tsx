e71c3b6a0761cd429b7a8f30a1d089d8f6da7fc6
Jeremy <jere.arde@gmail.com>
2023-04-24 21:45:23 -0700

    login test on frontend

--- ui-astro/src/components/Login.tsx
+++ ui-astro/src/components/Login.tsx
@@ -3,28 +3,31 @@ import React, { useState } from 'react';
 // import { CreateSurveyRequest } from "../../../server/bindings/CreateSurveyRequest";
 // import { supabase } from "../supabase";
 
-export default async function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [loggedin, setLoggedIn] = useState(false);
+export default function Login() {
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+    // const [loggedin, setLoggedIn] = useState(false);
 
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
-    }
+    // const handleEmailChange = (event) => {
+    //     setEmail(event.target.value);
+    // }
 
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    }
+    // const handlePasswordChange = (event) => {
+    //     setPassword(event.target.value);
+    // }
 
     const handleSubmit = (event) => {
         event.preventDefault();
-
+        console.log('handling submit');
+        // const data = {
+        //     email: email,
+        //     password: password
+        // };
         const data = {
-            email: email,
-            password: password
+            email: event.target.email.value,
+            password: event.target.password.value
         };
-
-        let results = fetch('/login', {
+        let results = fetch('http://localhost:8080/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -34,31 +37,28 @@ export default async function Login() {
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                setLoggedIn(true);
+                // setLoggedIn(true);
             })
             .catch(error => {
                 console.error(error);
             });
     }
 
-    return (
-        <>
-            loggedin ?  {"We are logged in already"} :
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Email:</label>
-                    <input type="email" value={email} onChange={handleEmailChange} />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input type="password" value={password} onChange={handlePasswordChange} />
-                </div>
-                <div>
-                    <button type="submit">Login</button>
-                </div>
-            </form>
 
-        </>
-    )
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Email:</label>
+                <input type="email" name="email" />
+            </div>
+            <div>
+                <label>Password:</label>
+                <input type="password" name="password" />
+            </div>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    );
 }
 
