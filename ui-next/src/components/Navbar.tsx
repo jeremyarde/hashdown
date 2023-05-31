7ee022a0b766e8e4cad8422bdd3608cc18bde39a
Jeremy <jere.arde@gmail.com>
2023-05-30 21:45:04 -0700

    more css updates

--- ui-next/src/components/Navbar.tsx
+++ ui-next/src/components/Navbar.tsx
@@ -4,17 +4,19 @@ export function Navbar() {
     let navlinks = [{ title: "pricing", id: 1, link: "/pricing" }, { title: "login", id: 2, link: "/login" }];
 
     return (
-        <div className="container debug">
-            <nav className="navbar debug">
-                <a href="/" className="debug" style={{ display: "flex" }}>
-                    <img src="ui-next/src/app/favicon.ico"></img>
-                    <span className="">Formit</span>
-                </a>
+        <div className="container">
+            <nav className="navbar">
+                <h1>
+                    <a href="/" className="" style={{ display: "flex" }}>
+                        <img src="ui-next/src/app/favicon.ico"></img>
+                        <span className="">Formit</span>
+                    </a>
+                </h1>
 
                 <ul className="navbar-links">
                     {navlinks.map((nav, index) => (
-                        <li key={nav.id} className="">
-                            <a href={nav.link} className="">{nav.title}</a>
+                        <li key={nav.id} className="navbar-items">
+                            <a href={nav.link} className="navbar-items">{nav.title}</a>
                             <a></a>
                         </li>
                     ))}
