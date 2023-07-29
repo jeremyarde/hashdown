5c91b2fae59b13ad9595387c6a236a6e14e8e0dc
Jeremy <jere.arde@gmail.com>
2023-07-29 12:17:03 -0700

    fix tailwind not working, survey rerender issue

--- ui-vite/src/App.tsx
+++ ui-vite/src/App.tsx
@@ -12,14 +12,24 @@ import { markdown_to_form_wasm } from '../../backend/pkg/markdownparser';
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from './components/ui/form'
 import { Input } from './components/ui/input'
 import { RadioGroup, RadioGroupItem } from './components/ui/radio-group';
+import { Textarea } from './components/ui/textarea';
 // import { RadioGroup, RadioGroupItem } from './components/ui/radio-group'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formtext, setFormtext] = useState('# A survey title here\n- q1\n  - option 1\n  - option 2\n  - option 3\n- question 2\n  - q2 option 1\n  - q2 option 2"');
+  const survey = markdown_to_form_wasm(formtext);
 
   return (
     <>
-      <GenericForm></GenericForm>
+      <div className='grid grid-cols-2 gap-5'>
+        <Textarea className='w-full' cols={10} value={formtext} onChange={(evt) => {
+          let value = evt.target.value;
+          console.log(`Set text value: ${JSON.stringify(value)}`);
+          setFormtext(value);
+        }} />
+        <GenericForm key={'testkey'} plaintext={formtext} survey={survey}></GenericForm>
+
+      </div>
     </>
   )
 }
@@ -42,14 +52,15 @@ function App() {
 
 
 
-function GenericForm() {
-  let [survey, setSurvey] = useState(markdown_to_form_wasm("# A survey title here\n- q1\n  - option 1\n  - option 2\n  - option 3\n- question 2\n  - q2 option 1\n  - q2 option 2"));
+function GenericForm({ plaintext, survey }) {
+  // let [survey, setSurvey] = useState(markdown_to_form_wasm("# A survey title here\n- q1\n  - option 1\n  - option 2\n  - option 3\n- question 2\n  - q2 option 1\n  - q2 option 2"));
   // let [survey, setSurvey] = useState(markdown_to_form_wasm("# A survey title here\n- q1\n  - option 1\n  - option 2"));
+  // let [survey, setSurvey] = useState(markdown_to_form_wasm(plaintext));
   // setSurvey(() => markdown_to_form_wasm("# title\n- q1\n  - option 1"));
 
   // let schema = survey.questions.map();
 
-
+  // const survey = markdown_to_form_wasm(plaintext);
   let objects = {};
   let defaultValues = {};
   survey.questions.forEach(element => {
@@ -58,22 +69,9 @@ function GenericForm() {
     objects[element.id] = z.enum(element.options.map((option) => option.text), { required_error: "Please select an option" });
     defaultValues[element.id] = "";
   });
-
-  // console.log(`survey: ${JSON.stringify(survey)}`);
-  // console.log(`default: ${JSON.stringify(defaultValues)}`);
-  // console.log(`objects: ${JSON.stringify(objects)}`);
-  // console.log(`keys: ${JSON.stringify(Object.keys(objects))}`);
-
-
   const formSchema = z.object(objects);
-  // const formSchema = z.object({ username: z.string() });
 
-  // const form = useForm<z.infer<typeof formSchema>>({
-  //   resolver: zodResolver(formSchema),
-  //   defaultValues: {
-  //     username: "",
-  //   },
-  // });
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: defaultValues,
@@ -92,46 +90,52 @@ function GenericForm() {
    * @param {Question} question 
    */
   function formelements(question) {
-    return (<FormField
-      key={question.id}
-      control={form.control}
-      name={question.id}
-      render={({ field }) => {
+    return (
+      <FormField
+        key={question.id}
+        control={form.control}
+        name={question.id}
+        className="flex text-left"
+        render={({ field }) => {
 
 
-        // return question.options.map((option) => {
-        return (
-          <FormItem>
-            <FormLabel>{question.value}</FormLabel>
-            <FormControl>
-              <RadioGroup
-                onValueChange={field.onChange}
-                defaultValue={field.value}
-                className="flex flex-col space-y-1"
-              >
-                {question.options.map((option) => {
-                  return (
-                    <FormItem
-                      className="flex items-center space-x-3 space-y-0"
-                    >
-                      <FormControl>
-                        <RadioGroupItem value={option.text} />
-                      </FormControl>
-                      <FormLabel className="font-normal">
-                        {option.id}: {option.text}
-                      </FormLabel>
-                    </FormItem>
-                  )
-                })}
-              </RadioGroup>
-            </FormControl>
-            <FormDescription>
-              {question.id}
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )
-      }} />)
+          // return question.options.map((option) => {
+          return (
+            <FormItem>
+              <FormLabel>{question.value}</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                // className="flex flex-col space-y-6"
+                >
+                  {question.options.map((option) => {
+                    return (
+                      <FormItem
+                      // className="flex items-center space-x-3 space-y-0"
+                      >
+                        <FormControl>
+                          <RadioGroupItem value={option.text} />
+                        </FormControl>
+                        <FormLabel
+                        // className="font-normal"
+                        >
+                          {option.id}: {option.text}
+                        </FormLabel>
+                      </FormItem>
+                    )
+                  })}
+                </RadioGroup>
+              </FormControl>
+              <FormDescription>
+                {question.id}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )
+        }}
+      />
+    )
   }
 
 
@@ -140,72 +144,20 @@ function GenericForm() {
 
 
   return (
-    <>
+    <><div className='flex flex-col items-start'>
+      <h2>id: {survey.id}</h2>
       <h2>{survey.title}</h2>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6 flex ">
+        <form onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6 justify-start items-start"
+        >
           {survey.questions.map((question) =>
             formelements(question))
           }
-          <Button type="submit" > Submit</Button>
+          <Button className="" type="submit" >Submit</Button>
         </form>
       </Form >
-      {/* {JSON.stringify(x)}
-      {JSON.stringify(survey, null, 2)} */}
-
-      {/* <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="items"
-          render={() => (
-            <FormItem>
-              <div className="mb-4">
-                <FormLabel className="text-base">Sidebar</FormLabel>
-                <FormDescription>
-                  Select the items you want to display in the sidebar.
-                </FormDescription>
-              </div>
-              {items.map((item) => (
-                <FormField
-                  key={item.id}
-                  control={form.control}
-                  name="items"
-                  render={({ field }) => {
-                    return (
-                      <FormItem
-                        key={item.id}
-                        className="flex flex-row items-start space-x-3 space-y-0"
-                      >
-                        <FormControl>
-                          <Checkbox
-                            checked={field.value?.includes(item.id)}
-                            onCheckedChange={(checked) => {
-                              return checked
-                                ? field.onChange([...field.value, item.id])
-                                : field.onChange(
-                                    field.value?.filter(
-                                      (value) => value !== item.id
-                                    )
-                                  )
-                            }}
-                          />
-                        </FormControl>
-                        <FormLabel className="font-normal">
-                          {item.label}
-                        </FormLabel>
-                      </FormItem>
-                    )
-                  }}
-                />
-              ))}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form> */}
+    </div>
     </>)
 }
 
