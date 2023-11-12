032cfdae8d24567c89d6b7563cccc5f9367aa139
Jeremy <jere.arde@gmail.com>
2023-11-11 22:52:32 -0800

    manual state setting for form

--- ui-vite/src/RenderedForm.tsx
+++ ui-vite/src/RenderedForm.tsx
@@ -19,27 +19,27 @@ export type RenderedFormProps = {
 }
 
 export function RenderedForm({ plaintext, survey }: RenderedFormProps) {
-    let [submittedValues, setSubmittedValues] = useState(null);
+    const DISPLAY_SURVEY_VERSION = 1;
 
-    const { survey_id } = useParams<{ survey_id: string }>()
+    // let [submittedValues, setSubmittedValues] = useState(null);
 
-    // let [form, setForm] = useState(undefined);
+    // const { survey_id } = useParams<{ survey_id: string }>()
 
-    console.log(`render - survey: ${JSON.stringify(survey)}`)
-    let objects = {};
-    let defaultValues = {};
-    const questions = survey?.questions ?? [];
-    questions.forEach(element => {
-        // console.log(element);
-        // objects[element.id] = z.string().min(10, { message: "Hey, longer please" });
-        objects[element.id] = z.enum(element.options.map((option) => option.text), { required_error: "Please select an option" });
-        defaultValues[element.id] = "";
-    });
-    const formSchema = z.object(objects);
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: defaultValues,
-    });
+    // // let [form, setForm] = useState(undefined);
+
+    // console.log(`render - survey: ${JSON.stringify(survey)}`)
+    // let objects = {};
+    // let defaultValues = {};
+    // const questions = survey?.questions ?? [];
+    // questions.forEach(element => {
+    //     objects[element.id] = z.enum(element.options.map((option) => option.text), { required_error: "Please select an option" });
+    //     defaultValues[element.id] = "";
+    // });
+    // const formSchema = z.object(objects);
+    // const form = useForm<z.infer<typeof formSchema>>({
+    //     resolver: zodResolver(formSchema),
+    //     defaultValues: defaultValues,
+    // });
 
     async function onSubmit(values: z.infer<typeof formSchema>) {
         // Do something with the form values.
@@ -111,7 +111,6 @@ export function RenderedForm({ plaintext, survey }: RenderedFormProps) {
                                     </RadioGroup>
                                 </FormControl>
                             </div>
-
                             <FormMessage />
                         </FormItem>
                     );
@@ -121,40 +120,53 @@ export function RenderedForm({ plaintext, survey }: RenderedFormProps) {
 
     let toRender;
     if (survey) {
-        toRender = (<>
-            <div className='flex flex-col justify-start items-start w-full'>
-                <h1 className='w-full text-center'>{survey.title}</h1>
-                {/* <Badge className="bg-gray-800 text-white" variant="outline">{survey.survey_id}</Badge> */}
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)}
-                        className="space-y-6 w-full"
-                    >
-                        {survey.questions.map((question) => formelements(question))}
-                        <Button className="bg-slate-400 rounded hover:bg-slate-150" type="submit">Submit</Button>
-                    </form>
-                </Form>
-            </div>
-            {/* {
-                submittedValues && (
-                    <>
-                        <h3>Submitted Values</h3>
-                        <Alert className={'bg-green-300'}>
-                            <AlertDescription>
-                                {submittedValues ? JSON.stringify(submittedValues) : ''}
-                            </AlertDescription>
-                        </Alert>
-                    </>
-                )
-            } */}
-            <div>
-                {ExampleForm()}
-            </div>
-        </>)
+        if (DISPLAY_SURVEY_VERSION == 0) {
+            toRender = (
+                <>
+                    <div className='flex flex-col justify-start items-start w-full'>
+                        <h1 className='w-full text-center'>{survey.title}</h1>
+                        {/* <Badge className="bg-gray-800 text-white" variant="outline">{survey.survey_id}</Badge> */}
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)}
+                                className="space-y-6 w-full"
+                            >
+                                {survey.questions.map((question) => formelements(question))}
+                                <Button className="bg-slate-400 rounded hover:bg-slate-150" type="submit">Submit</Button>
+                            </form>
+                        </Form>
+                    </div>
+                    {
+                        submittedValues && (
+                            <>
+                                <h3>Submitted Values</h3>
+                                <Alert className={'bg-green-300'}>
+                                    <AlertDescription>
+                                        {submittedValues ? JSON.stringify(submittedValues) : ''}
+                                    </AlertDescription>
+                                </Alert>
+                            </>
+                        )
+                    }
+                </>
+            )
+        } else {
+            toRender = (
+                <>
+                    <div className=''>
+                        <h1 className='w-full text-center'>{survey.title}</h1>
+                        {renderSurvey(survey)}
+                    </div>
+                </>
+            );
+        }
     } else {
         <div>Not available</div>
     }
     return (toRender)
 }
+
+
+
 
 
 /**
@@ -169,65 +181,191 @@ import { CardContent, Card } from "./components/ui/card"
 import { Badge } from "./components/ui/badge";
 import { BASE_URL } from "./lib/constants";
 import { useParams } from "react-router-dom";
+import { render } from "react-dom";
 
-export function ExampleForm() {
+
+export interface SurveyModel {
+    title: string
+    plaintext: string
+    questions: Question[]
+    parse_version: string
+    survey_id: string
+}
+
+export interface Question {
+    id: string
+    value: string
+    options: Option[]
+    type: "Text" | "Dropdown" | "Radio" | "Checkbox"
+    created_on: string
+    modified_on: string
+}
+
+export interface Option {
+    id: string
+    text: string
+}
+
+function radioGroup(question: Question, setStateFn) {
+    const onchangeexample = (evt) => {
+        console.log('onchangeexample');
+
+        // console.log(evt.target);
+        setStateFn(prev => ({
+            ...prev,
+            [question.id]: evt.target.value,
+        }));
+    }
+    return (
+        <div className="space-y-2">
+            <Label className="font-semibold">{question.value}</Label>
+            <RadioGroup onChange={onchangeexample} >
+                {question.options.map((option: Option) => {
+                    return (
+                        <>
+                            <div className="flex items-center space-x-2">
+                                <div>{question.id}</div>
+                                <RadioGroupItem id={option.id} value={option.text} />
+                                <Label className="" htmlFor={option.id} >{option.text}</Label >
+                            </div>
+                        </>
+                    )
+                })}
+            </RadioGroup>
+        </div>
+    )
+}
+
+function checkboxGroup(question: Question) {
+    return (
+        <>
+            <Label className="font-semibold">{question.value}</Label>
+            <div className="flex flex-col space-y-2">
+                {question.options.map((option) => {
+                    return (
+                        <div className="flex items-center">
+                            <Checkbox id={option.id} name={option.id} required />
+                            <Label className="ml-2 text-sm" htmlFor={option.id}>
+                                {option.text}
+                            </Label>
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    )
+}
+
+function dropdownGroup(question: Question) {
+    return (
+        <>
+            <Label htmlFor="dropdown">{question.value}</Label>
+            <Select required>
+                <SelectTrigger id="dropdown">
+                    <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent>
+                    {question.options.map((option) => {
+                        return (
+                            <SelectItem value={option.text}>{option.text} </SelectItem>
+                        )
+                    })}
+                </SelectContent>
+            </Select>
+        </>
+    )
+}
+
+function textInput(question: Question) {
+    return (
+        <>
+            <Label htmlFor={question.id}>{question.value}</Label>
+            <Input id={question.id} placeholder="Enter text" />
+        </>
+    )
+}
+
+function onchange(evt) {
+    evt.preventDefault();
+    console.log('jere/ onchange')
+    console.log(evt.target);
+}
+function renderSurvey(survey: SurveyModel) {
+    const initialState = survey.questions.map((question: Question) => [question.id, '']);
+    console.log('initial: ' + JSON.stringify(initialState));
+    const [formstate, setFormstate] = useState(Object.fromEntries(initialState));
+    console.log('formstate: ' + JSON.stringify(formstate));
+
+    // console.log(`jere/ formstate: ` + formstate);
+    async function onSubmit(evt) {
+        evt.preventDefault();
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        const formdata = new FormData(evt.target);
+        console.log(`submit:`);
+        console.log(evt.target);
+        console.log(JSON.stringify(formdata));
+        console.log(formdata.get("myform"));
+
+
+        // const surveySubmission = {
+        //     survey_id: survey.survey_id ?? '',
+        //     responses: values
+        // }
+        // // setSubmittedValues((prev) => surveySubmission);
+
+        // if (survey.survey_id) {
+        //     const response = await fetch(`${BASE_URL}/submit`, {
+        //         method: "POST",
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //         },
+        //         credentials: 'include',
+        //         body: JSON.stringify(surveySubmission),
+        //     });
+        //     console.log(`submit response: ${JSON.stringify(response)}`);
+        // } else {
+        //     console.log("Not sending submittion");
+        // }
+    }
+
     return (
         <Card>
             <CardContent>
-                <form className="space-y-4">
-                    <div className="space-y-2">
-                        <Label htmlFor="textInput">Text Input</Label>
-                        <Input id="textInput" placeholder="Enter text" />
-                    </div>
-                    <div className="space-y-2">
-                        <Label htmlFor="dropdown">Dropdown</Label>
-                        <Select>
-                            <SelectTrigger id="dropdown">
-                                <SelectValue placeholder="Select" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value="option1">Option 1</SelectItem>
-                                <SelectItem value="option2">Option 2</SelectItem>
-                                <SelectItem value="option3">Option 3</SelectItem>
-                            </SelectContent>
-                        </Select>
-                    </div>
-                    <div className="space-y-2">
-                        <Label className="font-semibold">Checkbox Options</Label>
-                        <div className="flex flex-col space-y-2">
-                            <div className="flex items-center">
-                                <Checkbox id="option1" name="checkbox" />
-                                <Label className="ml-2 text-sm" htmlFor="option1">
-                                    Option 1
-                                </Label>
+                <form className="space-y-4"
+                    // action="http://localhost:3000/api/submit"
+                    // method="post"
+                    // encType="application/x-www-form-urlencoded"
+                    onSubmit={(evt) => onSubmit(evt)}
+                    id="myform"
+                // id={survey.survey_id}
+                >
+                    {survey.questions.map((question: Question) => {
+                        let renderedQuestion = undefined;
+                        switch (question.type) {
+                            case "Radio":
+                                renderedQuestion = radioGroup(question, setFormstate);
+                                break;
+                            case "Checkbox":
+                                renderedQuestion = checkboxGroup(question);
+                                break;
+                            case "Dropdown":
+                                renderedQuestion = dropdownGroup(question);
+                                break;
+                            case "Text":
+                                renderedQuestion = textInput(question);
+                                break;
+                            default:
+                                renderedQuestion = (<div>Not a question type</div>);
+                                break;
+                        }
+                        return (
+                            <div key={question.id} className="space-y-2">
+                                {renderedQuestion}
                             </div>
-                            <div className="flex items-center">
-                                <Checkbox id="option2" name="checkbox" />
-                                <Label className="ml-2 text-sm" htmlFor="option2">
-                                    Option 2
-                                </Label>
-                            </div>
-                            <div className="flex items-center">
-                                <Checkbox id="option3" name="checkbox" />
-                                <Label className="ml-2 text-sm" htmlFor="option3">
-                                    Option 3
-                                </Label>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="space-y-2">
-                        <Label className="font-semibold">Radio Options</Label>
-                        <RadioGroup defaultValue="option1">
-                            <Label className="flex items-center space-x-2" htmlFor="option1">
-                                <RadioGroupItem id="option1" value="option1" />
-                                <span>Option 1</span>
-                            </Label>
-                            <Label className="flex items-center space-x-2" htmlFor="option2">
-                                <RadioGroupItem id="option2" value="option2" />
-                                <span>Option 2</span>
-                            </Label>
-                        </RadioGroup>
-                    </div>
+                        )
+                    })}
+                    <Button className="bg-slate-400 rounded hover:bg-slate-150" type="submit">Submit</Button>
                 </form>
             </CardContent>
         </Card>
