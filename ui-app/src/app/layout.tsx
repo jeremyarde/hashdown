c1cf110cd0ccdba32cc9ac593172caf6bc8c89a6
Jeremy <jere.arde@gmail.com>
2024-10-24 22:15:58 -0700

    add router

--- ui-app/src/app/layout.tsx
+++ ui-app/src/app/layout.tsx
@@ -1,12 +1,16 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
+import { AppCrumbs } from "@/components/app-crumbs";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
       <main>
-        <SidebarTrigger />
+        <div className="flex flex-row items-center justify-between">
+          <SidebarTrigger />
+          <AppCrumbs />
+        </div>
         {children}
       </main>
     </SidebarProvider>
---
+++ ui-app/src/components/app-editor.tsx
@@ -0,0 +1,33 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { Textarea } from "./ui/textarea";
+
+export function AppEditor() {
+  return (
+    <>
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>File</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>New Window</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Share</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Print</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+      <Textarea />
+    </>
+  );
+}
